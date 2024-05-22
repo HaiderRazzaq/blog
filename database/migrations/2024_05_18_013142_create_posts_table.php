d519e12cdf5745e06594fcdafac2d9f9d1413ca2
Haider Razzaq Dell <haiderraz77@gmail.com>
2024-05-22 03:05:07 +0300

    posts table done

--- database/migrations/2024_05_18_013142_create_posts_table.php
+++ database/migrations/2024_05_18_013142_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('body');
+            $table->string('slug')->unique();
+            $table->strng('image');
             $table->timestamps();
         });
     }
